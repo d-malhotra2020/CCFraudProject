9fdeb291102477b4c4147a7ffbf71cfcacb0e885
Dhruv Malhotra <dhruvmalhotra1986@gmail.com>
2021-12-21 14:21:12 -0600

    added ip address extractor

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+import json
+from urllib.request import urlopen
 
 app = Flask(__name__)
-
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def extract_ip_lonlat():
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
+    ip = data['ip']
+    loc = data['loc']
+    return 'IP Address: ' + ip + ' Long/Lat: ' + loc
 
-
-if __name__ == '__main__':
-    app.run()
+def extract_address():
